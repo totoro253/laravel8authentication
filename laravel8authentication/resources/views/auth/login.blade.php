96ff8b97ea0f1865bf0454d76edde608ae762a98
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-12-16 14:13:31 +0100

    added insertfields for email and password

--- laravel8authentication/resources/views/auth/login.blade.php
+++ laravel8authentication/resources/views/auth/login.blade.php
@@ -2,7 +2,30 @@
 @section('title','Login')
 
 @section('content')
-<h1>Login page</h1>
+<div class="container-fluid">
+    <div class="row d-flex justify-content-center align-items-center min-vh-100">
+        <div class="col-lg-4">
+            <div class="card shadow">
+                <div class="card-header">
+                    <h2 class="fw-bold text-secondary">Login</h2>
+                </div>
+                <div class="card-body p-5">
+                    <form actions="#" method="POST" id="login_form">
+                        @csrf  
+                        <div class="mb-3">
+                            <input type="email" name="email" id="email" class="form-control rounded-0" placeholder="Email">
+                            <div class="invalid-feedback"></div>
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" name="password" id="password" class="form-control rounded-0" placeholder="Password">
+                            <div class="invalid-feedback"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('script')
